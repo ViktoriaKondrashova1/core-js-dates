0cae35b0ddc2eccbcb96e88a4c915fe06888a834
Viktoria Kondrashova <kavidmi@gmail.com>
2025-03-03 17:59:38 +0300

    feat: solve three tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -119,8 +119,11 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const timeSart = new Date(dateStart).getTime();
+  const timeEnd = new Date(dateEnd).getTime();
+  const msInDay = 1000 * 60 * 60 * 24;
+  return (timeEnd - timeSart) / msInDay + 1;
 }
 
 /**
@@ -140,8 +143,11 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const start = new Date(period.start);
+  const end = new Date(period.end);
+  const targetDate = new Date(date);
+  return targetDate >= start && targetDate <= end;
 }
 
 /**
@@ -155,8 +161,24 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const dateObj = new Date(date);
+  const day = dateObj.getUTCDate();
+  const month = dateObj.getUTCMonth();
+  const year = dateObj.getUTCFullYear();
+  const hours = dateObj.getUTCHours();
+  const minutes =
+    dateObj.getUTCMinutes() < 10
+      ? `0${dateObj.getUTCMinutes()}`
+      : dateObj.getUTCMinutes();
+  const seconds =
+    dateObj.getUTCSeconds() < 10
+      ? `0${dateObj.getUTCSeconds()}`
+      : dateObj.getUTCSeconds();
+  const timeOfDay = hours >= 12 ? 'PM' : 'AM';
+  const formattedHours = hours % 12 || 12;
+
+  return `${month + 1}/${day}/${year}, ${formattedHours}:${minutes}:${seconds} ${timeOfDay}`;
 }
 
 /**
